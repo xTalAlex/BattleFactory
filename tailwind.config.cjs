8c77a05dd0b9e2e2d9553cf622becf45d68f30bf
xTalAlex <talale93@hotmail.it>
2022-08-29 06:44:18 +0200

    fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,11 @@ const colors = require('tailwindcss/colors');
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['Ubuntu', 'sans-serif'],
+			'serif': ['Merriweather', 'serif'],
+			'cursive': ['Goldman', 'curisve']
+		},
 		extend: {
 			colors: { 
                 primary: colors.orange,
