334931c805ce9cbe267f86af559c7e2434257483
xTalAlex <talale93@hotmail.it>
2022-08-29 10:44:57 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-
+import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
-
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-import astroI18next from "astro-i18next";
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import astroI18next from 'astro-i18next';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // output: 'server',
   // adapter: netlify(),
+  site: 'https://battlefactory.netlify.app',
   experimental: {
     integrations: true,
   },
@@ -19,5 +19,15 @@ export default defineConfig({
     }),
     vue(),
     astroI18next(),
+    sitemap({
+      filter: (page) => page !== 'https://battlefactory.netlify.app/404',
+      i18n : {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          it: 'it',
+        },
+      }
+    })
   ],
 });
